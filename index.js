ac9505c50fec8e921e4a0313c0bfb85cd26d5b44
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-04-11 21:34:24 -0400

    Define API routes to get timestamps based on dates or UNIX time integers

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -18,15 +19,26 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api', (req, res) => {
+    const currentDate = new Date();
+    res.json({unix: currentDate.getTime(), utc: currentDate.toUTCString()});  
 });
 
+app.get('/api/:date', (req, res) => {
+  const parsedUnixValue = parseInt(req.params.date);
+  const parsedDate = new Date(req.params.date);
+  console.log(parsedDate);
 
+  if(parsedDate.toString() === 'Invalid Date') {
+    res.json({error: parsedDate.toString()});
+  } else if (!Number.isNaN(parsedUnixValue)) {
+    res.json({unix: parsedUnixValue, utc: parsedDate.toUTCString()});
+  } else {
+    res.json({unix: parsedDate.toUTCString(), utc: parsedDate});
+  }
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log(`Your app is listening on port ${listener.address().port}`);
 });
